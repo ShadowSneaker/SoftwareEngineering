6da9a52b85854850c24928b92643a5c734278d78
ShadowSneaker <danielsfoyle@gmail.com>
2020-01-28 00:48:06 +0000

    Commit Test
    +Added Renderer File

---
+++ SoftwareEngineering/Game/Engine/Graphics/Renderer/Renderer.cpp
@@ -0,0 +1,14 @@
+#include "Renderer.h"
+
+
+
+CRenderer::CRenderer()
+{
+
+}
+
+
+CRenderer::~CRenderer()
+{
+
+}
---
+++ SoftwareEngineering/Game/Engine/Graphics/Renderer/Renderer.h
@@ -0,0 +1,34 @@
+#pragma once
+
+
+
+// 
+class CRenderer
+{
+private:
+	/// Properties
+
+
+
+
+public:
+	/// Constructors
+
+	// Constructor, Default.
+	CRenderer();
+
+	// Destructor
+	~CRenderer();
+
+
+
+	/// Functions
+
+
+
+	/// Setters
+
+
+
+	/// Getters
+}
