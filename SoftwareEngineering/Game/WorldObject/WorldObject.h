2836b4bfcb615e4f9ebad57f5ec5733a1291458f
c0zza <corrieanderson98@hotmail.com>
2020-02-17 15:43:21 +0000

    WorldObject ID management

--- SoftwareEngineering/Game/WorldObject/WorldObject.h
+++ SoftwareEngineering/Game/WorldObject/WorldObject.h
@@ -1,24 +1,32 @@
 #pragma once
 
 #include <CopiriteMath/Datatypes/Vector.h>
+#include <string>
+#include <vector>
 
 class WorldObject
 {
 private:
 
-	void		validateHeading(); // Keeps heading within 0 - 360 degrees
+	static std::vector<WorldObject*>	worldObjects;
+	static int							worldObjectIdIndex;
+
+	void	validateHeading(); // Keeps heading within 0 - 360 degrees
 
 protected:
 
 	SVector3	m_position;
 	SVector2	m_scale;
-	float		m_heading;
+	float		m_heading = 0.0f;
 
-	// ToBeDefined	m_worldObjectID;
+	int			m_ID;
 
 	bool		m_isActive;
 
 public:
+
+	WorldObject();
+	WorldObject(SVector3 position = { 0.0f, 0.0f, 0.0f }, float heading = 0.0f, SVector2 scale = { 1.0f, 1.0f }, int worldObjectId = -1, bool isActive = true);
 
 	SVector3	Position();
 	void		SetPosition(SVector3 position);
@@ -33,11 +41,14 @@ public:
 	void		SetHeading(float heading);
 	void		Rotate(float rotation);
 
-	// ToBeDefined ID();
-
 	bool		IsActive();
 	void		SetIsActive(bool isActive);
 	void		ToggleIsActive();
+
+	int			GetId();
+
+	static WorldObject*	GetWorldObjectById(int id);
+	static int NumberOfWorldObjects();
 
 };
 
