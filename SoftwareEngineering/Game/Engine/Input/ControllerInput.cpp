5a355314e592f4a4a5ac9b3a1421a14ffb8bf47e
Keron320 <32654913+Keron320@users.noreply.github.com>
2020-03-07 14:11:09 +0000

    ControllerInputClass

---
+++ SoftwareEngineering/Game/Engine/Input/ControllerInput.cpp
@@ -0,0 +1,61 @@
+#include "ControllerInput.h"
+#include <stdio.h>
+#include <iostream>
+
+bool ControllerInput::Initialization()
+{
+    //Initialization flag
+    bool success = true;
+
+    //Initialize SDL
+    if (SDL_Init(SDL_INIT_JOYSTICK < 0))
+    {
+        printf("SDL could not initialize! SDL Error: %s\n", SDL_GetError());
+        success = false;
+    }
+
+    //Check for joysticks
+    if (SDL_NumJoysticks() < 1)
+    {
+        printf("Warning: No joysticks connected!\n");
+    }
+    else
+    {
+        //Load joystick
+        Controller = SDL_JoystickOpen(0);
+        if (Controller == NULL)
+        {
+            printf("Warning: Unable to open game controller! SDL Error: %s\n", SDL_GetError());
+        }
+    }
+
+    return success;
+}
+
+ControllerInput::ControllerInput()
+{
+
+}
+
+ControllerInput::~ControllerInput()
+{
+}
+
+void ControllerInput::ReceiveInput(SDL_Event* event)
+{
+    if (event->type == SDL_JOYAXISMOTION)
+    {
+        if (event->jaxis.which == 0)
+        {
+            if (event->jaxis.axis == 0)
+            {
+                std::cout << event->jaxis.value << std::endl;
+            }
+        }
+    }
+}
+
+void ControllerInput::Something()
+{
+}
+
---
+++ SoftwareEngineering/Game/Engine/Input/ControllerInput.h
@@ -0,0 +1,21 @@
+#include "SDL_gamecontroller.h"
+#include "SDL.h"
+#pragma once
+class ControllerInput
+{
+private:
+	SDL_Joystick* Controller = NULL;
+	const int JOYSTICK_DEAD_ZONE = 8000;
+	int xDirection = 0;
+	int yDirection = 0;
+public:
+	bool Initialization();
+	void ReceiveInput(SDL_Event* event);
+	void Something();
+	~ControllerInput();
+	ControllerInput();
+
+
+
+};
+
